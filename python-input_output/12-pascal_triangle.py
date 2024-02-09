791cfa38f4a385b30bc593d9e98a7608255e4fa9
Nacho <146840051+rivero04@users.noreply.github.com>
2024-02-09 16:09:25 -0300

    Tasks Input/Output

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,4 +10,17 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+    triangle = [[1]]
 
+    for i in range(1, n):
+        last_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, i):
+            new_value = last_row[j-1] + last_row[j]
+            new_row.append(new_value)
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
