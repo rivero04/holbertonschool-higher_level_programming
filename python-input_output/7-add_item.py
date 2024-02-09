64447ff2c25a9606516534096a1db508bf90a22c
Nacho <146840051+rivero04@users.noreply.github.com>
2024-02-09 15:50:06 -0300

    Tasks Input/Output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,17 +4,16 @@ Script that adds all arguments to a Python list.
 """
 
 
-import os
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-
-if os.path.exists(filename):
+try:
     my_list = load_from_json_file(filename)
-else:
+except Exception:
     my_list = []
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[1])
+    save_to_json_file(my_list, filename)
