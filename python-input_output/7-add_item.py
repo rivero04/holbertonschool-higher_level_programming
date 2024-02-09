cc6ed49081a17d5b7c39992aec9e1e54a9e19e28
Nacho <146840051+rivero04@users.noreply.github.com>
2024-02-09 15:59:32 -0300

    Tasks Input/Output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,10 +12,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 try:
     my_list = load_from_json_file(filename)
-    for i in range(1, len(sys.argv)):
-        list.append(sys.argv[i])
 except Exception:
     my_list = []
     for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
+        my_list.append(sys.argv[1])
     save_to_json_file(my_list, filename)
