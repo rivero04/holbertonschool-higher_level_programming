c49ad13493c0c33f32b2007b98bb2e8f49ab77c1
rivero04 <7710@holbertonstudents.com>
2024-02-12 07:53:58 -0800

    task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,9 @@ class Rectangle(Base):
         """
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle instance.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
