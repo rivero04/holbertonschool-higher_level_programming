40946ea56cda4f1dd11ba6589bcd4025ccb7f207
rivero04 <7710@holbertonstudents.com>
2024-02-13 05:09:15 -0800

    task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,3 +22,27 @@ class Square(Rectangle):
         """
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
