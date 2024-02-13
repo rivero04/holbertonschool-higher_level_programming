73eb82ed948c456958be85181c93a392e7df1981
rivero04 <7710@holbertonstudents.com>
2024-02-13 05:01:36 -0800

    task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Create Class Base"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Constructor of the Square class.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square instance.
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
