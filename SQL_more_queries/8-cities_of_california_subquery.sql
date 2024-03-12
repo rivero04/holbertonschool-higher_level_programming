f25e27b7db393465a6876cc0c3a5c1f6cc11c8e8
rivero04 <7710@holbertonstudents.com>
2024-03-12 06:13:45 -0700

    wait to fix mña

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,15 +1,9 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT cities.name
-
--- Selecting the name of the cities
 FROM cities
-
--- Filtering cities by the state_id that matches California's id
 WHERE cities.state_id = (
     SELECT states.id
     FROM states
     WHERE states.name = 'California'
 )
-
--- Ordering the results by city id in ascending order
 ORDER BY cities.id ASC;
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,5 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name,
-
--- Subquery to get the state name for each city
 (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
-
 FROM cities
-
--- Ordering the results by city id in ascending order
 ORDER BY cities.id ASC;
