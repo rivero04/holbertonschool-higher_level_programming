1f1bd151c8b49fc68de3444706fefb7ed6411acc
rivero04 <7710@holbertonstudents.com>
2024-03-12 05:19:40 -0700

    fix task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- script that creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
