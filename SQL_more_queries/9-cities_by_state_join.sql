7b6464227fce7f536cafe00538f89c29120c2bbb
rivero04 <7710@holbertonstudents.com>
2024-03-13 06:47:39 -0700

    fix task 9?

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name,
-(SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
-FROM cities
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
