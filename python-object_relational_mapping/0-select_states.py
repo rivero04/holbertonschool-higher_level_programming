97ecdd3e907f135fb4920695a64b1b38e1bc57f9
rivero04 <7710@holbertonstudents.com>
2024-03-14 04:43:00 -0700

    fix task 0?

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+Lists all states from the `hbtn_0e_0_usa` database, ordered by id.
 """
 
 import MySQLdb
 from sys import argv
 
-user = argv[1]
-password = argv[2]
-db_name = argv[3]
+if __name__ == '__main__':
+    """
+    Access the database and get the status
+    from the database.
+    """
 
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=user, passwd=password, db=db_name)
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
 
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=password, db=db_name)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
 # uso fetchall para recuperar los registros restantes que quedaron en cursor
-for row in cursor.fetchall():
-    print(row)
+    for row in cursor.fetchall():
+        print(row)
