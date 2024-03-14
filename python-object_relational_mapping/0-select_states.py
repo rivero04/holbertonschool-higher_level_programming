6e794d4db52ab78b22dc9a3e16cd1944cfb93247
rivero04 <7710@holbertonstudents.com>
2024-03-14 04:38:23 -0700

    fix task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,16 @@ database `hbtn_0e_0_usa`.
 import MySQLdb
 from sys import argv
 
-user = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
+user = argv[1]
+password = argv[2]
+db_name = argv[3]
 
-db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, db=db_name)
+db = MySQLdb.connect(host="localhost", port=3306,
+                     user=user, passwd=password, db=db_name)
+
 cursor = db.cursor()
-
 cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-print("id | name")
-print("---|------")
-for row in cursor.fetchall(): #uso fetchall para recuperar los registros restantes que quedaron en cursor
-    print(f"{row[0]} | {row[1]}")
+# uso fetchall para recuperar los registros restantes que quedaron en cursor
+for row in cursor.fetchall():
+    print(row)
