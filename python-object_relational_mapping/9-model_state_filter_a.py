e644df5c80bbe62e5c820e0100eb717b0c0e1953
rivero04 <7710@holbertonstudents.com>
2024-03-15 04:42:25 -0700

    pycode 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all State objects containing the letter 'a' 
+script that lists all State objects containing the letter 'a'
 from the database hbtn_0e_6_usa
 """
 
